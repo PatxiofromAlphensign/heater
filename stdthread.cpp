3f31a56df5fcde3b63c4fa8e4e5410634c2bc764
worthless443 <aissypewdiepie@gmail.comn>
2022-05-05 12:32:47 +0530

    using std lib

---
+++ stdthread.cpp
@@ -0,0 +1,32 @@
+#include<thread>
+#include<vector>
+#include<functional>
+#include<iostream>
+
+class thread {
+	std::vector<std::jthread> threads;
+	public:
+	thread() = default;
+	thread(int count, float (*f)(int, float, float)) {
+		for(int i=0;i<count;i++) {
+			threads.push_back(std::move(std::jthread(std::bind(f, count,1,1))));
+		}
+	}
+	void join() {
+		for(std::jthread &thread : threads ) if(thread.joinable()) thread.join();
+	}
+};
+
+float compute(int size, float count=1, float count1=1)  {
+	if(count>size) return count;
+	for(int i=0;i<10;i++)
+		count = compute(size, count+1);
+	count1 = 1/count;
+	std::cout << count;
+	return count + ((count + count*2)/(count1 + count*2));
+}
+
+int main() {
+	thread t(100000, compute);
+	t.join();
+}
